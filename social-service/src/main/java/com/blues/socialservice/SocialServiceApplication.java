c047c3822ae58ef93c9d2968100178c19cc8546b
truongnguyen-kt <1234561662002kt@gmail.com>
2026-07-07 12:53:01 +0700

    Setup complete: Cấu trúc Microservices đa module

---
+++ social-service/src/main/java/com/blues/socialservice/SocialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blues.socialservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialServiceApplication.class, args);
+    }
+
+}
---
+++ trader-service/src/main/java/com/blues/traderservice/TraderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blues.traderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraderServiceApplication.class, args);
+    }
+
+}
